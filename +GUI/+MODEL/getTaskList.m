78965a47e6fabeed9e4f683e85f9e39fdafc72ce
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-03-17 18:37:06 +0100

    changed task order in the GUI

--- +GUI/+MODEL/getTaskList.m
+++ +GUI/+MODEL/getTaskList.m
@@ -13,10 +13,10 @@ function TaskList = getTaskList()
 % TaskList = TaskList_raw(Task_idx);
 
 TaskList = {
-    'Landscapes_Encoding_Immediate' 'Language_Encoding_Immediate' 'Objects_Encoding_Immediate'
     'Landscapes_Encoding_Deferred'  'Language_Encoding_Deferred'  'Objects_Encoding_Deferred'
-    'Landscapes_Recall_Immediate'   'Language_Recall_Immediate'   'Objects_Recall_Immediate'
     'Landscapes_Recall_Deferred'    'Language_Recall_Deferred'    'Objects_Recall_Deferred'
+    'Landscapes_Encoding_Immediate' 'Language_Encoding_Immediate' 'Objects_Encoding_Immediate'
+    'Landscapes_Recall_Immediate'   'Language_Recall_Immediate'   'Objects_Recall_Immediate'
 };
 
 end % function
