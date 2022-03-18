a3fd65443425ba56197a2e4da79272a3d84b88aa
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-03-18 09:39:11 +0100

    Objects: ok

--- +TASK/Graphics.m
+++ +TASK/Graphics.m
@@ -5,14 +5,14 @@ function p = Graphics( p )
 
 p.FixationCross.Size     = 0.10;              %  Size_px = ScreenY_px * Size
 p.FixationCross.Width    = 0.10;              % Width_px =    Size_px * Width
-p.FixationCross.Color    = [127 127 127 255]; % [R G B a], from 0 to 255
+p.FixationCross.Color    = [000 000 000 255]; % [R G B a], from 0 to 255
 p.FixationCross.Position = [0.50 0.50];       % Position_px = [ScreenX_px ScreenY_px] .* Position
 
 
 %% Text
 
 p.Text.Size   = 0.20;              % TextSize = ScreenY_px * Size
-p.Text.Color  = [127 127 127 255]; % [R G B a], from 0 to 255
+p.Text.Color  = [000 000 000 255]; % [R G B a], from 0 to 255
 p.Text.Center = [0.5 0.5];         % Position_px = [ScreenX_px ScreenY_px] .* Position
 
 
--- +TASK/Keybindings.m
+++ +TASK/Keybindings.m
@@ -8,24 +8,11 @@ switch S.Environement
     
     case 'MRI' %-----------------------------------------------------------
         
-        switch Task
-            
-            case 'Language'
-                S.Keybinds.TaskSpecific.Catch = KbName('b'); % blue in right hand
-            case 'Landscapes'
-                S.Keybinds.TaskSpecific.Catch = KbName('b'); % blue in right hand
-                
-        end
+        S.Keybinds.TaskSpecific.Catch = KbName('b'); % blue in right hand
         
     case 'Keyboard' %------------------------------------------------------
         
-        switch Task
-            
-            case 'Language'
-                S.Keybinds.TaskSpecific.Catch = KbName('RightArrow');
-            case 'Language'
-                S.Keybinds.TaskSpecific.Catch = KbName('RightArrow');
-        end
+        S.Keybinds.TaskSpecific.Catch = KbName('RightArrow');
         
 end
 
