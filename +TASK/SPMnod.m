058bbbcd41d75f2fcb7fd9858e6837da480b8661
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-03-18 09:55:50 +0100

    click is now managed in the SPMnod

--- +TASK/SPMnod.m
+++ +TASK/SPMnod.m
@@ -13,6 +13,7 @@ try
     names = {
         'baseline'
         'activation'
+        'click'
         };
     
     % 'onsets' & 'durations' for SPM
@@ -52,6 +53,55 @@ try
             durations{num.(EventData{event,1})} = [ durations{num.(EventData{event,1})} ; EventData{event+1,2}-EventData{event,2}] ;
         end
         
+    end
+    
+    %% Add Clicks to SPM model input
+    
+    keys = cellstr( KbName(struct2array(S.Keybinds.TaskSpecific)) );
+    
+    for f = 1:length(keys)
+        click_spot.(keys{f}) = regexp(S.KL.KbEvents(:,1),keys{f});
+        click_spot.(keys{f}) = ~cellfun(@isempty,click_spot.(keys{f}));
+        click_spot.(keys{f}) = find(click_spot.(keys{f}));
+    end
+    click_onset    = [];
+    click_duration = [];
+    
+    count = 0 ;
+    for f = 1:length(keys)
+        
+        count = count + 1 ;
+        
+        if ~isempty(S.KL.KbEvents{click_spot.(keys{f}),2})
+            click_idx = cell2mat(S.KL.KbEvents{click_spot.(keys{f}),2}(:,2)) == 1;
+            click_idx = find(click_idx);
+            % if only 1 keypres and never released (wtf ?), need to deal with this unprobable case
+            if size(S.KL.KbEvents{click_spot.(keys{f}),2}) < 3
+                S.KL.KbEvents{click_spot.(keys{f}),2}{click_idx(end),3} = [];
+            end
+            % the last click can be be unfinished : button down + end of stim = no button up
+            if isempty(S.KL.KbEvents{click_spot.(keys{f}),2}{click_idx(end),3})
+                S.KL.KbEvents{click_spot.(keys{f}),2}{click_idx(end),3} =  S.ER.Data{end,2} - S.KL.KbEvents{click_spot.(keys{f}),2}{click_idx(end),1};
+            end
+            click_onset    = [ click_onset    ; cell2mat(S.KL.KbEvents{click_spot.(keys{f}),2}(click_idx,1)) ];
+            click_duration = [ click_duration ; cell2mat(S.KL.KbEvents{click_spot.(keys{f}),2}(click_idx,3)) ];
+        else
+            % pass
+        end
+        
+    end
+    
+    % reorder
+    [click_onset,i] = sort(click_onset,'ascend');
+    click_duration = click_duration(i);
+    
+    onsets   {num.click} = click_onset;
+    durations{num.click} = click_duration;
+    
+    if isempty(onsets{num.click})
+        names    (num.click) = [];
+        onsets   (num.click) = [];
+        durations(num.click) = [];
     end
     
     
